87e171ad1ce443e656f7aef89fed205d6eccf698
Liam Hockley <xdega121@gmail.com>
2020-04-18 10:57:54 -0700

    Implement custom logo

--- functions.php
+++ functions.php
@@ -24,6 +24,7 @@ class StarterSite extends Timber\Site {
         add_filter('timber_context', [$this, 'add_to_context']);
         add_action('widgets_init', [$this, 'register_sidebars']);
         add_action('init', [$this, 'register_menus']);
+        add_action('after_setup_theme', [$this, 'custom_logo_support']);
         parent::__construct();
     }
 
@@ -39,7 +40,16 @@ class StarterSite extends Timber\Site {
         $context['menu_footer'] = new Timber\Menu('Footer Menu');
         $context['site'] = $this;
 
+        // Custom Logo
+        $custom_logo_id = get_theme_mod('custom_logo');
+        $custom_logo_url = wp_get_attachment_image_url($custom_logo_id, 'full');
+        $context['custom_logo_url'] = $custom_logo_url;
+
         return $context;
+    }
+
+    public function custom_logo_support() {
+        add_theme_support('custom-logo');
     }
 
     public function theme_supports() {
