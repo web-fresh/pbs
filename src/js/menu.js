252a9c7918e4cbdb3f55436b81fad0b1b3e041ae
Liam Hockley <xdega121@gmail.com>
2020-05-25 09:46:48 -0700

    Update menu toggle arrow animation

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,19 @@
 export function toggleMenu() {
   const menu = document.getElementById("menuMain");
+  const toggle = document.getElementById("menuToggle");
+
   menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+  toggle.classList.toggle("rotate-icon");
 }
 
 export function deactivateToggle() {
   if (window.innerWidth > 450) {
     const menu = document.getElementById("menuMain");
     const menuHidden = menu.style.display === "none";
+    const toggle = document.getElementById("menuToggle");
+
+
     if (menuHidden) toggleMenu();
+    toggle.classList.toggle("rotate-icon");
   }
 }
